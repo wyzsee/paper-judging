f2f70b9118ae3f4a7ae0097305dfc2c714992bf1
wyzsee <wessewess1@gmail.com>
2026-01-14 15:05:01 +0700

    Update page title to "Paper Judging" for consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Sistem Penjurian",
+  title: "Paper Judging",
   description: "Aplikasi Penjurian Makalah Online",
 };
 
